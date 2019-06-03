f858269a3452e9bb4ac2367ae969987abbc23ed2
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-06-03 13:18:31 +0300

    Lesson 7

--- src/test/java/petstore/endpoints/PetEndpoint.java
+++ src/test/java/petstore/endpoints/PetEndpoint.java
@@ -1,24 +1,32 @@
 package petstore.endpoints;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import petstore.models.PetModel;
 
+
 public class PetEndpoint {
+    Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
     private RequestSpecification given() {
-        return RestAssured.given()
+        SerenityRest.enableLoggingOfRequestAndResponseIfValidationFails();
+        return SerenityRest.given()
                 .baseUri(Config.BASE_URI)
-                .contentType("application/json")
-                .log().uri();
+                .contentType("application/json");
+                //.log().uri();
     }
 
+    @Step
     public ValidatableResponse getPetById(int petId) {
+        log.info("Executing: getPetById");
         return given()
                 .get(Config.GET_PET_BY_ID, petId)
-                .then()
-                .log().all();
+                .then();
+                //.log().all();
     }
 
     public enum Status {
@@ -27,35 +35,39 @@ public class PetEndpoint {
         SOLD
     }
 
+    @Step
     public ValidatableResponse getPetByStatus(Status status) {
         return given()
                 .param("status", status)
                 .get(Config.GET_PET_BY_STATUS)
-                .then()
-                .log().all();
+                .then();
+                //.log().all();
     }
 
+    @Step
     public ValidatableResponse createPet(PetModel petModel) {
         return given()
                 .body(petModel)
                 .post(Config.CREATE_PET)
-                .then()
-                .log().all();
+                .then();
+                //.log().all();
     }
 
+    @Step
     public ValidatableResponse deletePet(int petId) {
         return given()
                 .delete(Config.DELETE_PET_BY_ID, petId)
-                .then()
-                .log().all();
+                .then();
+                //.log().all();
     }
 
+    @Step
     public ValidatableResponse updatePet(PetModel petModel) {
         return given()
                 .body(petModel)
                 .put(Config.UPDATE_PET_BY_ID)
-                .then()
-                .log().all();
+                .then();
+                //.log().all();
     }
 
 
--- src/test/java/petstore/tests/PetStoreTest.java
+++ src/test/java/petstore/tests/PetStoreTest.java
@@ -1,6 +1,9 @@
 package petstore.tests;
 
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import petstore.endpoints.PetEndpoint;
 import petstore.models.CategoryModel;
 import petstore.models.PetModel;
@@ -8,8 +11,9 @@ import petstore.models.TagModel;
 
 import static petstore.endpoints.PetEndpoint.*;
 
-
+@RunWith(SerenityRunner.class)
 public class PetStoreTest {
+    @Steps
     private PetEndpoint petEndpoint = new PetEndpoint();
 
     @Test
